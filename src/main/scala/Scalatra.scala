5d055ca4691f9b329ed85b7c37831402e4f48c80
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-07-29 15:13:57 +0200

    make start

--- src/main/scala/Scalatra.scala
+++ src/main/scala/Scalatra.scala
@@ -6,7 +6,7 @@ class Scalatra extends LifeCycle with init.DatabaseInit {
 
   override def init(context: ServletContext) {
     configureDb()
-    context mount (new Articles, "/articles*")
-    context mount (new Users, "/users*")
+    context mount (new Articles, "/articles/*")
+    context mount (new Users, "/users/*")
   }
 }
--- src/main/scala/com/futurechimps/squeryli/init/DatabaseSessionSupport.scala
+++ src/main/scala/com/futurechimps/squeryli/init/DatabaseSessionSupport.scala
@@ -2,10 +2,10 @@ package com.futurechimps.squeryli.init
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import org.scalatra.Handler
 import org.squeryl.Session
 import org.squeryl.SessionFactory
 import scala.util.DynamicVariable
+import org.scalatra._
 
 object DatabaseSessionSupport {
   val key = {
@@ -14,7 +14,7 @@ object DatabaseSessionSupport {
   }
 }
 
-trait DatabaseSessionSupport { self: ScalatraBase =>
+trait DatabaseSessionSupport { this: ScalatraBase =>
   import DatabaseSessionSupport._
 
   def dbSession = request.get(key).orNull.asInstanceOf[Session]
@@ -29,17 +29,17 @@ trait DatabaseSessionSupport { self: ScalatraBase =>
     dbSession.unbindFromCurrentThread
   }
 
-  private val _dbSession = new DynamicVariable[Session](null)
-  def dynVarSession = _dbSession.value
-  abstract override def handle(req: HttpServletRequest, res: HttpServletResponse) {
-    dbSession.withValue(SessionFactory.newSession) {
-      dbSession.value.bindToCurrentThread
-      try {
-        super.handle(req, res)
-      } finally {
-        dbSession.value.close
-        dbSession.value.unbindFromCurrentThread
-      }
-    }
-  }
+  // private val _dbSession = new DynamicVariable[Session](null)
+  // def dynVarSession = _dbSession.value
+  // abstract override def handle(req: HttpServletRequest, res: HttpServletResponse) {
+  //   dbSession.withValue(SessionFactory.newSession) {
+  //     dbSession.value.bindToCurrentThread
+  //     try {
+  //       super.handle(req, res)
+  //     } finally {
+  //       dbSession.value.close
+  //       dbSession.value.unbindFromCurrentThread
+  //     }
+  //   }
+  // }
 }
