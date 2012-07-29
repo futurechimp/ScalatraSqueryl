7bfd2322238134d3f17a731d736d8a72722517be
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-07-29 13:29:43 +0100

    Taking a lot of oauth2-server specific stuff.

--- src/main/Scalatra.scala
+++ src/main/Scalatra.scala
@@ -1,21 +1,7 @@
 class Scalatra extends LifeCycle {
 
-  implicit var system: ActorSystem = null
   override def init(context: ServletContext) {
-
-    system = context.getOrElseUpdate(ActorSystemContextKey, ActorSystem(ActorSystemName)).asInstanceOf[ActorSystem]
-    OAuth2Extension(system)
-
-    context mount (new Articles, "/articles")
-    context mount (new Users, "/users")
-  }
-
-  override def destroy(context: ServletContext) {
-    system.synchronized {
-      if (system != null && !system.isTerminated) {
-        system.shutdown()
-        system.awaitTermination(30 seconds)
-      }
-    }
+    context mount (new Articles, "/articles*")
+    context mount (new Users, "/users*")
   }
 }
